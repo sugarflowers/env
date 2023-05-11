5a5c0ec615c78401300b1faeef2a40be711ef8d9
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2023-05-11 16:55:09 +0900

    modify function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
 use std::env;
 
 pub fn get_env(key: &str) -> String {
-    match env::var(key) {
-        Ok(val) => val,
-        Err(e) => panic!("{}", e),
-    }
+    env::var(key).unwrap_or("".to_string())
 }
 
+#[test]
+fn all_var() {
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
+}
+#[test]
+fn get_test() {
+    println!(">>{}", get_env("USERPROFILE"));
+    println!(">>{}", get_env("INVALIDKEY"));
+}
